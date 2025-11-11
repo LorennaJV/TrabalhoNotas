2fd57344a2a9f6fa0d35cf777fb2a5784f102795
LorennaJV <lorenna.vieira.ifpr.2023@gmail.com>
2025-11-11 18:17:30 -0300

    Refactor adicionar and apagarTudo functions

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 var corAtual = "azul"
 
 function adicionar(tipo) {
-  var texto = document.getElementById("textoNota").value;
+  var texto = document.getElementById("textoNota").value
 
-  var nota = document.createElement("div");
-  nota.classList.add("nota", corAtual);
-  nota.textContent = texto;
+  var nota = document.createElement("div")
+  nota.classList.add("nota", corAtual)
+  nota.textContent = texto
 
 
   if (corAtual === "azul") {
-    corAtual = "verde";
+    corAtual = "verde"
   } else if (corAtual === "verde") {
-    corAtual = "roxo";
-  } else {
-    corAtual = "azul";
+    corAtual = "roxo"
+  } 
+  else {
+    corAtual = "azul"
   }
 
  
-  var botaoRemover = document.createElement("button");
-  botaoRemover.textContent = "Remover";
-  botaoRemover.className = "remover";
+  var botaoRemover = document.createElement("button")
+  botaoRemover.textContent = "Remover"
+  botaoRemover.className = "remover"
   botaoRemover.onclick = function() {
-    nota.remove();
-  };
+    nota.remove()
+  }
 
-  nota.appendChild(botaoRemover);
+  nota.appendChild(botaoRemover)
 
 
   if (tipo === "urgente") {
-    document.getElementById("Urgente").appendChild(nota);
+    document.getElementById("Urgente").appendChild(nota)
   } else {
-    document.getElementById("NaoUrgente").appendChild(nota);
+    document.getElementById("NaoUrgente").appendChild(nota)
   }
 
-  document.getElementById("textoNota").value = '';
+  document.getElementById("textoNota").value = " "
 }
 
 function apagarTudo() {
-  document.getElementById("Urgente").innerHTML = '';
-  document.getElementById("NaoUrgente").innerHTML = '';
+  document.getElementById("Urgente").innerHTML = " "
+  document.getElementById("NaoUrgente").innerHTML = " "
 }
-
